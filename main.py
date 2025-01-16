3aee4948def836da2bbb65b468e0139d6cb118bb
senapol <sensen.zp@gmail.com>
2025-01-16 20:58:12 +0000

    'config'

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from ultralytics import YOLO
+import cv2
 import torch
 import os
 
+# YOLOv6n is fastest but least accurate model
+# mAP accuracy can be increased by training
+model = YOLO('yolov8n.pt')
 
+if __name__ == '__main__':
+    results = model.train(data="config.yaml", epochs=50, patience=5)
